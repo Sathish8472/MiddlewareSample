c106e225c85a5632bcdd38d9faa8cd00306d6381
SISA Sathish S <sathish.s@sisainfosec.com>
2023-07-11 09:26:07 +0530

    Add project files.

---
+++ EnvironmentMiddleware.cs
@@ -0,0 +1,18 @@
+﻿internal class EnvironmentMiddleware
+{
+    private RequestDelegate next;
+
+    public string EnvironmentName { get; }
+
+    public EnvironmentMiddleware(RequestDelegate next, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+    {
+        this.next = next;
+        this.EnvironmentName = env.EnvironmentName;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        Console.WriteLine($"This is Environment Middleware");
+        await next(context);
+    }
+}
